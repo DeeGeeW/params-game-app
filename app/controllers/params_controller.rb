0cbdf6caa89d5f2c822dbcec5aa8e41284d71767
DeeGeeWest <DeeGeeWest@gmail.com>
2022-03-09 21:02:53 -0500

    params

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,2 +1,45 @@
 class ParamsController < ApplicationController
+  # def query_params
+  #   name = params["name"]
+  #   if params["name"][1] == "a"
+  #     render json: {message: "Hey, your name starts with the first letter of the alphabet!"}
+  #   else
+  #     render json: {message: "cool!"}
+     
+  #  end
+  #   end
+  def upcase_params
+    name = params["name"].upcase
+    if name.starts_with?("A") 
+      render json: {message: "Hey, your name starts with the first letter of the alphabet!"}
+    else
+      render json: {message: "your name is #{name}"}
+    end
+  end
+
+  def guess_number
+    num_choice = params["user_number"].to_i
+    winning_num = 42
+    if num_choice < winning_num
+      render json: {message: "Too Low!"}
+    elsif num_choice > winning_num
+      render json: {message: "Too High!"}
+    else
+      render json: {message: "hooray!"}
+    end
+  end
+
+
+
 end
+
+#   def a_params
+#     name = params["name"]
+#        if name.starts_with?("A") 
+#          render json: {message: "Hey, your name starts with the first letter of the alphabet!"}
+#        else
+#          render json: {message: "cool!"}
+#        end
+       
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+get "/upcase_params", controller: "params", action: "upcase_params"
 
+get "/guess_number", controller: "params", action: "guess_number"
+# get "/a_params", controller: "params", action: "a_params"
   # Defines the root path route ("/")
   # root "articles#index"
 end
